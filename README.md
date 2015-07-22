8e346566c9defd50fc62d4797f755ffb79ebc8e5
Alexander Gugel <alexander.gugel@gmail.com>
2015-07-22 19:10:41 +0200

    Init

---
+++ README.md
@@ -0,0 +1,29 @@
+# wsd
+
+> = **W**eb**S**ocket **D**ebugger
+
+[![Build Status](https://travis-ci.org/alexander/wsd.svg?branch=master)](https://travis-ci.org/alexanderGugel/wsd)
+
+Simple command line utility for debugging WebSocket servers.
+
+## Installation
+
+Via `go-get`:
+
+```
+$ go get github.com/alexanderGugel/wsd
+```
+
+## Usage
+
+Command-line usage.
+
+## Why?
+
+Debugging WebSocket servers should be as simple as firing up `cURL`. No need
+for dozens of flags, just type `wsd -url=ws://localhost:1337/ws` and you're
+connected.
+
+## License
+
+ MIT
---
+++ main.go
@@ -0,0 +1,138 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"sync"
+
+	"github.com/fatih/color"
+	"golang.org/x/net/websocket"
+)
+
+// Current version number
+const Version = "0.0.0"
+
+var (
+	origin         string
+	url            string
+	protocol       string
+	displayHelp    bool
+	displayVersion bool
+	red            = color.New(color.FgRed).SprintFunc()
+	magenta        = color.New(color.FgMagenta).SprintFunc()
+	green          = color.New(color.FgGreen).SprintFunc()
+	yellow         = color.New(color.FgYellow).SprintFunc()
+	cyan           = color.New(color.FgCyan).SprintFunc()
+	wg             sync.WaitGroup
+)
+
+func init() {
+	flag.StringVar(&origin, "origin", "http://localhost/", "origin of Websocket client")
+	flag.StringVar(&url, "url", "ws://localhost:1337/ws", "WebSocket server address to connect to")
+	flag.StringVar(&protocol, "protocol", "", "WebSocket subprotocol")
+	flag.BoolVar(&displayHelp, "help", false, "Display help information about wsd")
+	flag.BoolVar(&displayVersion, "version", false, "Display version number")
+}
+
+func inLoop(ws *websocket.Conn, errors chan<- error, in chan<- []byte) {
+	var msg = make([]byte, 512)
+
+	for {
+		var n int
+		var err error
+
+		n, err = ws.Read(msg)
+
+		if err != nil {
+			errors <- err
+			continue
+		}
+
+		in <- msg[:n]
+	}
+}
+
+func printErrors(errors <-chan error) {
+	for err := range errors {
+		if err == io.EOF {
+			fmt.Printf("\r✝ %v - connection closed by remote\n", magenta(err))
+			os.Exit(0)
+		} else {
+			fmt.Printf("\rerr %v\n> ", red(err))
+		}
+	}
+}
+
+func printReceivedMessages(in <-chan []byte) {
+	for msg := range in {
+		fmt.Printf("\r< %s\n> ", cyan(string(msg)))
+	}
+}
+
+func outLoop(ws *websocket.Conn, out <-chan []byte, errors chan<- error) {
+	for msg := range out {
+		_, err := ws.Write(msg)
+		if err != nil {
+			errors <- err
+		}
+	}
+}
+
+func main() {
+	flag.Parse()
+
+	if displayVersion {
+		fmt.Fprintf(os.Stdout, "%s version %s\n", os.Args[0], Version)
+		os.Exit(0)
+	}
+
+	if displayHelp {
+		fmt.Fprintf(os.Stdout, "Usage of %s:\n", os.Args[0])
+		flag.PrintDefaults()
+		os.Exit(0)
+	}
+
+	ws, err := websocket.Dial(url, protocol, origin)
+
+	if protocol != "" {
+		fmt.Printf("connecting to %s via %s from %s...\n", yellow(url), yellow(protocol), yellow(origin))
+	} else {
+		fmt.Printf("connecting to %s from %s...\n", yellow(url), yellow(origin))
+	}
+
+	defer ws.Close()
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("successfully connected to %s\n\n", green(url))
+
+	wg.Add(3)
+
+	errors := make(chan error)
+	in := make(chan []byte)
+	out := make(chan []byte)
+
+	defer close(errors)
+	defer close(out)
+	defer close(in)
+
+	go inLoop(ws, errors, in)
+	go printReceivedMessages(in)
+	go printErrors(errors)
+	go outLoop(ws, out, errors)
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	fmt.Print("> ")
+	for scanner.Scan() {
+		out <- []byte(scanner.Text())
+		fmt.Print("> ")
+	}
+
+	wg.Wait()
+}
